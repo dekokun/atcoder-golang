472e61fea47014cfc156acb8b8ecaa297e961d91
dekokun <dekokun@hatena.ne.jp>
2019-09-05 21:15:56 +0900

    fit for golang

--- new_problem.sh
+++ new_problem.sh
@@ -17,8 +17,7 @@ fi
 program_name="$1"
 cargo new "$program_name" --bin
 git checkout -b "$program_name"
-cp -pr template/src/main.rs "$program_name"/src
+cp -pr template/main.go "$program_name"/
 cp -pr template/runtest "$program_name"/runtest
-cp -pr template/rust-toolchain "$program_name"/rust-toolchain.bak
 git add "$program_name"
 git commit -m"$program_name initialize"
