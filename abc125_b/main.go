0f191b11fcb080dd4f534e2b918b3ee4c0f02319
dekokun <dekokun@hatena.ne.jp>
2019-09-05 21:32:25 +0900

    abc125_b pass

--- abc125_b/main.go
+++ abc125_b/main.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	var a, b, t int
-	fmt.Scan(&a, &b, &t)
+	var n int
+	fmt.Scan(&n)
+	var num int
+	var values []int
+	for i := 0; i < n; i++ {
+		fmt.Scan(&num)
+		values = append(values, num)
+	}
+	var costs []int
+	for i := 0; i < n; i++ {
+		var num int
+		fmt.Scan(&num)
+		costs = append(costs, num)
+	}
 
-	fmt.Println((t / a) * b)
+	var realValues []int
+	for i := 0; i < n; i++ {
+		realValues = append(realValues, values[i]-costs[i])
+	}
+	var sum = 0
+	sort.Sort(sort.Reverse(sort.IntSlice(realValues)))
+	for _, realValue := range realValues {
+		if realValue > 0 {
+			sum += realValue
+		} else {
+			break
+		}
+	}
+
+	fmt.Println(sum)
 }
